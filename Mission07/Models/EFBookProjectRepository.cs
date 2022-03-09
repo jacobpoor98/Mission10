71ce30656ca1342eebd7c8221e99a8eb6ea2362b
Jacob Poor <jacob.poor98@gmail.com>
2022-03-09 09:48:37 -0700

    Initial

---
+++ Mission07/Models/EFBookProjectRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Mission07.Models
+{
+    // connects the IBookProjectRepository with the EFBookProjectRepository
+    public class EFBookProjectRepository : IBookProjectRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFBookProjectRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        // make it queryable
+        public IQueryable<Book> Books => context.Books;
+    }
+}
---
+++ Mission07/Models/IBookProjectRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+namespace Mission07.Models
+{
+    // creates the IBookProjectRepository and allows its contents to be queryable
+    public interface IBookProjectRepository
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
